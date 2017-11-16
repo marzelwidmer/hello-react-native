b13ab208ac12cdec0ac6441a1683fb785a7519ba
Marcel Widmer <marzelwidmer@gmail.com>
2017-11-16 22:50:47 +0100

    chatApp

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/components/Home'
+import Chat from './src/components/Chat '
+
+import{
+  Router,
+  Scene,
+} from 'react-native-router-flux'; 
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello Diana` aka. Rudi</Text>
-      </View>
+      <Router>
+        <Scene key='root'>
+          <Scene key='home' component={Home} title='Home' />
+          <Scene key='chat' component={Chat} title='Chat' />
+        </Scene>
+      </Router>        
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
